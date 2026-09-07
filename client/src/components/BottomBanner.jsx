443db47d5f153affa0136a079447b1b17da75cb6
sakshi0881 <khushi.8oct@gmail.com>
2026-09-08 00:02:54 +0530

    changed bottom banner

--- client/src/components/BottomBanner.jsx
+++ client/src/components/BottomBanner.jsx
@@ -32,17 +32,17 @@ const BottomBanner = () => {
       <img src={assets.bottom_banner_image} alt="banner" className='w-full hidden md:block' />
       <img src={assets.bottom_banner_image_sm} alt="banner" className='w-full md:hidden' />
 
-      <div className ='absolute inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24'>
+      <div className ='absolute inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-60'>
         <div>
-            <h1 className='text-2xl md:text-3xl font-semibold text-primary mb-6'>Why We Are the Best?</h1>
+            <h1 className='text-2xl md:text-3xl font-semibold text-black underline underline-offset-4 mb-6'>Why We Are the Best?</h1>
             {featureList.map((feature, index)=>(
                 <div key={index} className='flex items-center gap-4 mt-2'>
                     <div className="w-10 h-10 md:w-11 md:h-11 rounded-full bg-primary/10 flex items-center justify-center text-primary">
                         <HugeiconsIcon icon={feature.icon} size={24} />
                     </div>
                     <div>
-                        <h3 className='text-lg md:text-xl font-semibold'>{feature.title}</h3>
-                        <p className='text-gray-500/70 text-xs md:text-sm'>{feature.description}</p>
+                        <h3 className='text-lg md:text-xl font-semibold text-black bg-orange-100 px-2 py-1 rounded-md inline-block'>{feature.title}</h3>
+                        <p className='text-black text-xs md:text-sm'>{feature.description}</p>
                     </div>
                     
                 </div>
